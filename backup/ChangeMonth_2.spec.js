259d98a9abd768f6812845287660cc93da44fecf
KwesiEssel <dadzie_essel@hotmail.de>
2026-02-05 07:17:17 +0100

    Anpassung

--- backup/ChangeMonth_2.spec.js
+++ backup/ChangeMonth_2.spec.js
@@ -6,19 +6,24 @@ import { readMonthIndex } from "../selectors/getMonthIndex.js";
 test('Change month | Tenant BAUT', async ({ page }) => {
     await page.goto('https://app-dev-taap.azurewebsites.net/#/login', { waitUntil: 'domcontentloaded' });
     await page.waitForLoadState('networkidle');
-
-    //Login
+    /*
+    Login
+    */
     await page.click(Login.BENUTZERNAME);
     await page.keyboard.type('65700285');
     await page.click(Login.PASSWORD);
     await page.keyboard.type('Taap!1');
     await page.locator(Login.ANMELDEN_BUTTON).nth(1).click();
 
-    //Warten auf vollständigen Laden
+    /*
+    Warten auf vollständigen Laden
+     */
     await page.waitForLoadState('networkidle');
 
     await page.pause()
-    //Prüfen auf den Monat
+    /*
+    Prüfen auf den Monat
+     */
     const beforeIndex = await readMonthIndex(page, Dashboard.MONTH_NAME);
     await page.click(Dashboard.RIGHTBUTTON_CALENDAR);
 
