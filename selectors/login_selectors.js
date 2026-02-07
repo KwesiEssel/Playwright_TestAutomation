a5095bd162dd818780e27fdb3916cbc69aaee257
KwesiEssel <44833655+KwesiEssel@users.noreply.github.com>
2026-02-07 20:21:38 +0100

    Update login_selectors.js

--- selectors/login_selectors.js
+++ selectors/login_selectors.js
@@ -6,6 +6,6 @@ export class login_selectors {
     /*
     Login Daten
     */
-    static LOGIN_INPUT = '65700285';
-    static PASSWORD_INPUT = 'nSC@Lw$8x5';
+    static LOGIN_INPUT = 'LOGIN';
+    static PASSWORD_INPUT = 'PASSWORT';
 }
